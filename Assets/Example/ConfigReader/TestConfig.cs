640b9fd090f1f866ed86f00e9c8a6a73990c8c10
fansongy <fansongy@126.com>
2014-09-08 12:04:24 +0800

    add ObjectConfig model which read data from json

---
+++ Assets/Example/ConfigReader/TestConfig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TestConfig : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		AssetLoader.Get().LoadConfig("testConfig",this.onLoaded);
+
+	}
+
+	void onLoaded(string name, UnityEngine.Object obj, object callbackData)
+	{
+		string json = obj.ToString();
+		ObjectConfig config = new ObjectConfig();
+		config.initialize(json);
+		object 	t1 = config.getConfig("testBool");
+		string 	t2 =(string)config.getConfig("testString");
+		long 	t3 = (long)config.getConfig("testLong");
+		double 	t4 = (double)config.getConfig("testdouble");
+		string 	t5 = (string)config.getConfig("testStringInGroup",1);
+		long 	t6 = (long)config.getConfig("testLongInGroup");
+		Debug.Log("the Result :\n testBool:"+t1+"\ntestString:"+t2+"\ntestLong:"+t3+"\ntestdouble:"+t4+"\ntestStringInGroup:"+t5+"\ntestIntInGroup:"+t6);
+	}
+
+}
---
+++ Assets/UExtend/FileReader/ObjectConfig.cs
@@ -0,0 +1,59 @@
+﻿/*
+ * Read Json from string
+ * get common data and leveled data
+ * 
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ObjectConfig {
+
+	Dictionary<string,object> configTable = new Dictionary<string, object>();
+
+	public ObjectConfig() {}
+	~ObjectConfig() {}
+		
+	public virtual void initialize(string jsonString)
+	{
+		configTable = MiniJSON.Json.Deserialize(jsonString) as Dictionary<string,object>;
+	}
+
+	public object getConfig(string key,int level = 0)
+	{
+		if(configTable.Count == 0)
+		{
+			Log.Asset.Print("ObjectConfig::getConfig() - configTable is empty");
+			return null;
+		}
+		if(!configTable.ContainsKey(key))
+		{
+			Log.Asset.Print("ObjectConfig::getConfig() - don't have {0} key ",key);
+			return null;
+		}
+		else
+		{
+			object value = configTable[key];
+			if( value is List<object>)
+			{
+				var asDict = value as List<object>;
+				//leveled
+				if(asDict.Count-1 <level)
+				{
+					Log.Asset.Print("ObjectConfig::getConfig() - level: {0} is larger than Dict count: {1},key is {2} ",level,asDict.Count,key);
+					return null;
+				}
+				else
+				{
+					return asDict[level];
+				}
+			}
+			else
+			{
+				return configTable[key];
+			}
+		}
+	}
+		
+}
