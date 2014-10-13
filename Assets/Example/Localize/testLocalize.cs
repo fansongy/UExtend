4684c1488036ca4152202f4e4a4e66f5cd240569
fansongy <fansongy@126.com>
2014-10-13 14:33:20 +0800

    add call from main

--- Assets/Example/Localize/testLocalize.cs
+++ Assets/Example/Localize/testLocalize.cs
@@ -1,50 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class testLocalize : MonoBehaviour {
 
-	ObjectConfig languageText = null;
+	string title = "???";
+	string desc = "???";
+	string[] langs = { "CN","EN","FR","RU","Error" };
+	int curSelected = 0;
+
 
 	// Use this for initialization
 	void Start () {
-		AssetLoader.Get ().LoadConfig ("localize",this.onLoadText);
+		Localize.Get().setCurLang(langs[curSelected]);
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	void onLoadText(string name, Object obj, object callbackData)
+	void OnGUI()
 	{
-		languageText = new ObjectConfig();
-		languageText.initialize(obj.ToString());
-		Log.fansy.Print(getStringByTID("TID_TEST_LANGUAGE_TITLE","N"));
+		curSelected = GUI.SelectionGrid(new Rect(100,100,300,80),curSelected,langs,5);
+		Localize.Get().setCurLang(langs[curSelected]);
+		title = Localize.Get().getStringByTID("TID_TEST_LANGUAGE_TITLE");
+		desc = Localize.Get().getStringByTID("TID_TEST_LANGUAGE_DES");
+		GUILayout.Label(title);
+		GUILayout.Label(desc);
 	}
 
-	string getStringByTID(string tid,string lang)
-	{
-		if(languageText != null)
-		{
-			Dictionary<string,object> title =(Dictionary<string,object>)languageText.getConfig(tid);
-			Log.fansy.Print(title.ToString());
-			object result;
-			if(title.TryGetValue(lang,out result))
-			{
-				return result.ToString();
-			}
-			else
-			{
-				Log.Asset.Print("getStringByTID() - can't find the lang {0} in {1},check the config file",lang,tid); 
-				return "???";
-			}
-
-		}
-		else
-		{
-			Log.Asset.Print("getStringByTID() - can't find the languageText,makesure it's loaded"); 
-			return "???";
-		}
-	}
 }
--- Assets/UExtend/Localize/Localize.cs
+++ Assets/UExtend/Localize/Localize.cs
@@ -1,15 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Localize : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	string curLang = "CN";
+	ObjectConfig languageText = null;
+
+	private static Localize s_instance;
+
+	public static Localize Get()
+	{
+		if(!s_instance)
+		{
+			GameObject loc = new GameObject();
+			loc.name = "Localize";
+			s_instance = loc.AddComponent<Localize>();
+			s_instance.initData();
+			return s_instance;
+		}
+		return Localize.s_instance;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void initData()
+	{
+		setDefaultLang();
+		AssetLoader.Get ().LoadConfig ("localize",this.onLoadText);
+	}
+
+	void setDefaultLang()
+	{
+		setCurLang("CN");
+	}
+
+	public void setCurLang(string lang)
+	{
+		curLang = lang;
+	}
+
+	void onLoadText(string name, Object obj, object callbackData)
+	{
+		languageText = new ObjectConfig();
+		languageText.initialize(obj.ToString());
+	}
+
+	public string getStringByTID(string tid)
+	{
+		if(languageText != null)
+		{
+			Dictionary<string,object> title =(Dictionary<string,object>)languageText.getConfig(tid);
+			if(title == null)
+			{
+				Log.Asset.Print("getStringByTID() - can't find the tid {0} in lang {1}",tid,curLang);
+				return "???";
+			}
+			object result;
+			if(title.TryGetValue(curLang,out result))
+			{
+				return result.ToString();
+			}
+			else
+			{
+				Log.Asset.Print("getStringByTID() - can't find the lang {0} in {1},check the config file",curLang,tid); 
+				return "???";
+			}
+		}
+		else
+		{
+			Log.Asset.Print("getStringByTID() - can't find the languageText,makesure it's loaded"); 
+			return "???";
+		}
 	}
 }
